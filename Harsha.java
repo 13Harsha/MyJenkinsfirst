d2d9324df533006e0e9db689458836aacfc2a362
765612 <765612@PCIN456823.cts.com>
2019-12-10 11:36:14 +0530

    main

--- Harsha.java
+++ Harsha.java
@@ -1,32 +1,8 @@
-package practice;
-import java.util.*; 
-import java.lang.*; 
-  
-public class Harsha { 
-  
-    
-    public static HashMap<Integer, String> sortByValue(HashMap<Integer , String> ma) 
-    { 
-       List<Map.Entry<Integer, String> > list = 
-               new LinkedList<Map.Entry<Integer,String> >(ma.entrySet()); 
-       Collections.sort(list, new Comparator<Map.Entry<Integer , String> >() { 
-            public int compare(Map.Entry< Integer,String> o1,  
-                               Map.Entry< Integer,String> o2) 
-            { 
-        } 
-        return temp; 
-    } 
-    public static void main(String[] args) 
-    { 
-  
-        
+package none;
 
-        
-       
-        Map<Integer , String> m = sortByValue(map); 
-        for (Map.Entry<Integer , String> en : m.entrySet()) { 
-            System.out.println("Key = " + en.getKey() +  
-                          ", Value = " + en.getValue()); 
-        } 
-    } 
-} 
+public class main{
+	public static void main(String args[]) {
+		System.out.println("Hi Harsha");
+		System.out.println("Hi Harsha");
+	}
+}
